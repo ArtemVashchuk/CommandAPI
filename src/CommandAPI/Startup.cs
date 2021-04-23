5c1c6d28baf9310ee728212f241ba201a2c247c2
Artem <artem_vashchuk@symbotic.com>
2021-04-24 00:10:00 +0300

    Add real epository implementation

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -3,20 +3,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
-            services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandRepo>();
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+
+            services.AddControllers();
+
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
